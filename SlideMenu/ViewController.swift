8485683091fd95767d7a81d11521d0a0c3dcf24d
kudpig <shin.kudo.aa@gmail.com>
2021-04-22 21:54:27 +0900

    outlet接続の追加(View、BlurView、Constraint)

--- SlideMenu/ViewController.swift
+++ SlideMenu/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var sideView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
