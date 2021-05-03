0773d333c5ef2840cb3f988841ef863d5f26a0dc
kudpig <shin.kudo.aa@gmail.com>
2021-05-03 11:57:47 +0900

    スライドの実装

--- SlideMenu/ViewController.swift
+++ SlideMenu/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var blurView: UIVisualEffectView!
     
-    @IBOutlet weak var sideView: UIView!
+    @IBOutlet weak var sideView: UIView! {
+        didSet {
+            sideView.isUserInteractionEnabled = true
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -25,10 +29,56 @@ class ViewController: UIViewController {
         sideView.layer.shadowOffset = CGSize(width: 5, height: 0)
         
         viewConstraint.constant = -175
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.performd(sender:)))
+        view.addGestureRecognizer(panGesture)
     }
 
-
-    @IBAction func panPerformd(_ sender: UIPanGestureRecognizer) {
+    @objc func performd(sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began || sender.state == .changed {
+            
+            let sideView = self.sideView
+            
+            let translation = sender.translation(in: sideView).x
+            
+            if translation > 0 { // swipe right
+                
+                if viewConstraint.constant < 20 {
+                    UIView.animate(withDuration: 0.2) {
+                        self.viewConstraint.constant += translation / 10
+                        //self.view.layoutIfNeeded() 制約の更新について見本の動画では使用されていたが、あまりよろしくないという話もある？
+                    }
+                }
+                
+            } else {  // swipe left
+                
+                if viewConstraint.constant > -175 {
+                    UIView.animate(withDuration: 0.2) {
+                        self.viewConstraint.constant += translation / 10
+                        //self.view.layoutIfNeeded()
+                    }
+                }
+                
+            }
+           
+            
+        } else if sender.state == .ended {
+            
+            if viewConstraint.constant < -100 {
+                UIView.animate(withDuration: 0.2) {
+                    self.viewConstraint.constant = -175
+                    //self.view.layoutIfNeeded()
+                }
+                
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    self.viewConstraint.constant = 0
+                    //self.view.layoutIfNeeded()
+                }
+            }
+        }
+        
     }
     
 }
